b9b34e14e40c65b04ab60b9c45a2e49d9fef170b
next-hero <mdimranulhaque.8dec20@gmail.com>
2021-05-20 06:59:45 +0600

    day-1

---
+++ hello.js
@@ -0,0 +1,148 @@
+//datatypes in javascript-
+//six data type -
+//undefived
+//boolean
+//number
+//string
+//bigint
+//symbol
+// var myName ="imran"
+// var myName =5
+// var myName =true
+// var asd
+// console.log(typeof (myName))//boolean
+// console.log(myName)
+// console.log(asd)//undefined
+// console.log(typeof(10+"20"),(10+"20"))
+// console.log(typeof(9-"5"),(9 -"5"))
+// console.log(typeof("java"+"script"),"java"+" "+ "scirpt")
+// //number + string => string-
+// //number -string => number-
+// console.log(typeof(" "+" "))
+// console.log(typeof(" "+0))
+// console.log(typeof("vio"-"asd"))
+//string -string => type wlll be number.
+// console.log(typeof(false-true))
+//boolian +or - boolian => type will be number.;
+// NaN => not a Number.;
+//nan => not a number . ;
+// console.log(true+true);
+// console.log(false+true);
+//false means 0;
+//true means 1;
+//thats why string . wow thats good ;enough.;
+//diffenence between null vs undefined.;
+//null vs undefined ;
+//null vs undefined ;
+// var useless =null;
+// console.log(useless);
+// console.log(typeof(useless)); //js bug
+// var less = undefined;
+// console.log(less);
+// console.log(typeof less);
+// what is NaN=> not a number ;
+//what is nan => not a number ;
+//what is nan => not a number ;
+// console.log("object"-"objeact")
+// var myPhoneNumber = 01771203257;
+// var myName ="irman"
+// console.log(isNaN(myPhoneNumber) )
+// console.log(isNaN(myName))
+// //is not a number? is not a number ? true or false ,
+// if (isNaN(myPhoneNumber)) {
+//     console.log("ahsldg")
+// }else{
+//     console.log("not a numebr")
+// }
+
+// console.log(NaN === NaN)
+
+// console.log(Number.NaN === NaN)
+
+// console.log(object)
+
+// console.log(isNaN(NaN))
+
+// console.log(isNaN(Number.NaN))
+
+// console.log(isNaN(Number.isNaN(NaN)))
+
+// console.log(5+30)
+
+// console.log(5+20)
+
+//operator.
+
+//operand.
+
+//operand and operators 
+
+//expression.=> combinations of operand and operator.
+
+//assigment operators
+
+//arithmetic operators 
+
+//comparison operators 
+//logical operators
+//string operators
+//conditional (ternary ) operator
+//
+// var x=5;
+// var y=5;
+//variable ,operand, operator ,value,totaly expression end 
+
+// console.log(x==y)
+
+// console.log(`is both the x and y are equal : ${x==y}`)
+
+//arithmatic operators 
+
+//arithmatic operators
+
+//artithmatic operators
+
+//++x or --x ,X++,X--
+
+// var num =15;
+// console.log(++num)
+// var a =23;
+// var b=3;
+// console.log(a!=b)
+
+//&& || operators 
+//logical and operators.
+//operand is true if and only if all of its operand are true
+// var a=4;
+// var b=5;
+// console.log(a>b || b>3);
+//conjunction > &&
+//disjunction > ||
+// console.log(!true)
+// //string operator ( concatenation operators )
+// console.log("hello world")
+// console.log("hello" +" wordl")
+// var myName="irma"
+// console.log(myName +" this")
+
+//challege time-
+
+//sol
+// console.log(3**3)
+//it just power ,
+// console.log(3**2)
+// console.log(23+" e")
+//write a progaram swap two number
+// var a =4
+// var b =5
+// // output b=4,a=5
+// var c=b;
+
+// b=a;a=c;
+
+// console.log(a,b)
+// var a=5
+// var b=7
+// var [b,a]=[a,b]
+// console.log(a,b)
+//________________20may-21_______________
